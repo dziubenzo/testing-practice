f9decacb1bd674220ac80e9eb1e6827debe925fb
dziubenzo <dziubenzo@gmail.com>
2024-01-06 17:06:07 +0100

    Make capitalize() pass all tests

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-export function capitalize(string) {}
+export function capitalize(string) {
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
+}
+
+export function reverseString(string) {}
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { capitalize } from './main';
+import { capitalize, reverseString } from './main';
 
 // capitalize() function tests
 test('capitalize() works for lowercase strings', () => {
@@ -9,6 +9,6 @@ test('capitalize() works for uppercase strings', () => {
   expect(capitalize('TESTINGISFUN')).toBe('TESTINGISFUN');
 });
 
-test('capitalize() does not break when number strings are used', () => {
+test('capitalize() works for number strings', () => {
   expect(capitalize('45544')).toBe('45544');
 });
