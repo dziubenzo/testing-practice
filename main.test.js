82494574e8a522add8998462b6fe3534e85d26f2
dziubenzo <dziubenzo@gmail.com>
2024-01-06 17:12:53 +0100

    Write reverseString() tests

--- main.test.js
+++ main.test.js
@@ -1,6 +1,6 @@
 import { capitalize, reverseString } from './main';
 
-// capitalize() function tests
+// capitalize() function
 test('capitalize() works for lowercase strings', () => {
   expect(capitalize('saturday')).toBe('Saturday');
 });
@@ -11,4 +11,21 @@ test('capitalize() works for uppercase strings', () => {
 
 test('capitalize() works for number strings', () => {
   expect(capitalize('45544')).toBe('45544');
+});
+
+// reverseString() function
+test('reverseString() works for lowercase strings', () => {
+  expect(reverseString('saturday')).toBe('yadrutas');
+});
+
+test('reverseString() works for uppercase strings', () => {
+  expect(reverseString('QUOKKAISFUN')).toBe('NUFSIAKKOUQ');
+});
+
+test('reverseString() works for strings with numbers', () => {
+  expect(reverseString('fun3string6')).toBe('6gnirts3nuf');
+});
+
+test('reverseString() works for mixed strings', () => {
+  expect(reverseString('5test6JEST7')).toBe('7TSEJ6tset5');
 });
